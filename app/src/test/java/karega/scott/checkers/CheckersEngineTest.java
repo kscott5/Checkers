3daa5e62cd6b59099708b0a6de9e8aa519ee2da1
Karega K. Scott <kkscott@outlook.com>
2021-04-12 20:17:34 -0500

    Fix travis-ci

--- app/src/test/java/karega/scott/checkers/CheckersEngineTest.java
+++ app/src/test/java/karega/scott/checkers/CheckersEngineTest.java
@@ -3,6 +3,6 @@ package karega.scott.checkers;
 import org.junit.Test;
 import org.junit.Assert;
 
-public class CheckersEngineTest extends CheckersEngine  {
+public class CheckersEngineTest  {
     
 }
