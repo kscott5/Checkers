687bf4d4c47584c18c400f2e133adf2b0df4b5f1
Karega K. Scott <kkscott@outlook.com>
2021-04-13 16:26:43 -0500

    Unit test

--- app/src/test/java/karega/scott/checkers/CheckersEngineTest.java
+++ app/src/test/java/karega/scott/checkers/CheckersEngineTest.java
@@ -3,6 +3,17 @@ package karega.scott.checkers;
 import org.junit.Test;
 import org.junit.Assert;
 
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import android.content.Context;
+
+@RunWith(MockitoJUnitRunner)
 public class CheckersEngineTest  {
-    
+        @Mock Context context;
+        
+        @Test public void simple() {
+          Assert.assertNotNull(context);
+        }
 }
