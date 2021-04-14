a02db8da5eb3584aff1e00388b7f7c1053bbde67
Karega K. Scott <kkscott@outlook.com>
2021-04-14 14:27:56 -0500

    Simple unit test

--- app/src/test/java/karega/scott/checkers/test/CheckersBaseTest.java
+++
@@ -1,9 +0,0 @@
-package karega.scott.checkers.test;
-
-import karega.scott.checkers.BoardSquare;
-import karega.scott.checkers.BoardSquareInfo;
-import karega.scott.checkers.CheckersEngine;
-
-public abstract class CheckersBaseTest {
-    
-} // end CheckersBaseTest
