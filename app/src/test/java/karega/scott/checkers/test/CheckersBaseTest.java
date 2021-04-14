f6e6ab5e375c28ddae5394d9798e6d11ea03b066
Karega K. Scott <kkscott@outlook.com>
2021-04-14 14:04:49 -0500

    Pure unit test of logic

--- app/src/test/java/karega/scott/checkers/test/CheckersBaseTest.java
+++ app/src/test/java/karega/scott/checkers/test/CheckersBaseTest.java
@@ -1,90 +1,11 @@
 package karega.scott.checkers.test;
 
-import karega.scott.checkers.BoardGameEngine;
 import karega.scott.checkers.BoardSquare;
 import karega.scott.checkers.BoardSquareInfo;
-import karega.scott.checkers.CheckerBoardSquare;
 import karega.scott.checkers.CheckersEngine;
 
-import org.junit.Test;
-import org.junit.Assert;
-
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
-
-import android.content.Context;
-
-import org.junit.Before;
-import org.junit.After;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-@RunWith(MockitoJUnitRunner.class)
 public abstract class CheckersBaseTest {
-	// Class variables allow extended subclasses to build complex
-	// test case using the same engine and in some case existing
-	// test methods
-	@Mock protected Context context;
-	      protected CheckersEngineWrapper engine;
-		
-	public class CheckersEngineWrapper extends CheckersEngine {
-		public CheckersEngineWrapper(boolean vsDevice) {
-			super(vsDevice);
-		}
-		
-		@Override
-		public BoardSquareInfo getData(int row, int col) {
-			return super.getData(row, col);
-		}
-		
-		@Override
-		public BoardSquareInfo getData(int id) {
-			return super.getData(id);
-		}
-		
-		@Override
-		protected void switchPlayer() {
-			super.switchPlayer();
-		}
-
-		@Override
-		public boolean handleOnTouch(BoardSquare square) {
-			return super.handleOnTouch(square);
-		}
-		
-		@Override
-		public void moveSquare(BoardSquareInfo target) {
-			super.moveSquare(target);
-		}
-
-		@Override
-		protected void moveSquareForDevice() {
-			super.moveSquareForDevice();
-		}
-		
-		@Override
-		protected int getSize() {
-			return super.getSize();
-		}
-		
-		@Override
-		protected boolean isEmpty(int row, int col) {
-			return super.isEmpty(row, col);
-		}		
-	}
-	
-	public class CheckerBoardSquareMock extends CheckerBoardSquare {
-		protected BoardSquareInfo info;
-		
-		public CheckerBoardSquareMock(Context context, BoardSquareInfo info) {
-			super(context, info);
-		}
-		
-		@Override
-		public void invalidate() {}
-	}
+    protected CheckersEngine engine;
 
 	/**
 	 * Prepares the board for simulated game play.
@@ -100,8 +21,8 @@ public abstract class CheckersBaseTest {
 		
 		for(int id=0;id<63;id++) {
 			BoardSquareInfo square=engine.getData(id);
-			if(square.state == BoardGameEngine.LOCKED_STATE 
-					|| square.state == BoardGameEngine.EMPTY_STATE) continue;
+			if(square.state == CheckersEngine.LOCKED_STATE 
+					|| square.state == CheckersEngine.EMPTY_STATE) continue;
 			square.makeEmpty();
 		}
 	} // end clearGameBoard
@@ -120,25 +41,4 @@ public abstract class CheckersBaseTest {
 		
 		return count;
 	} // end countSquares
-	
-
-	/*
-	 * Always starts a new game
-	 * @see org.junit4.Before
-	 */
-	@Before
-	public void setUp() {
-		engine = new CheckersEngineWrapper(/*vsDevice*/ false);
-		
-		engine.newGame();
-	}
-	
-	/*
-	 * Always end the game
-	 * @see org.junit4.After
-	 */
-	@After
-	public void tearDown() {
-		engine.exitGame();
-	}
 } // end CheckersBaseTest
