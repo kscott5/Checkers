35ba1e708279ac025d8f915a576f56ffea0498fb
Karega K. Scott <kkscott@outlook.com>
2021-04-14 13:31:00 -0500

    Syntax missing semi-colon

--- app/src/test/java/karega/scott/checkers/test/CheckersEngineTest.java
+++ app/src/test/java/karega/scott/checkers/test/CheckersEngineTest.java
@@ -1,13 +1,13 @@
 package karega.scott.checkers.test;
 
-import karega.scott.checkers.CheckersEngine;;
+import karega.scott.checkers.CheckersEngine;
 
 import karega.scott.checkers.BoardSquareInfo;
 
 import org.junit.Test;
 import org.junit.Assert;
 
-import org.junit.Before
+import org.junit.Before;
 import org.junit.After;
 
 public class CheckersEngineTest {
