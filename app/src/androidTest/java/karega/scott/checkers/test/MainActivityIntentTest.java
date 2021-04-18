b9b33b87019d0aca0dcf069d462497147cffd4c0
Karega K. Scott <kkscott@outlook.com>
2021-04-18 18:56:00 -0500

    Initial medium UI test

---
+++ app/src/androidTest/java/karega/scott/checkers/test/MainActivityIntentTest.java
@@ -0,0 +1,11 @@
+package karega.scott.checkers.test;
+import karega.scott.checkers.MainActivity;
+
+import androidx.test.filters.MediumTest;
+
+import org.junit.Before;
+import org.junit.Test;
+
+@MediumTest
+public class MainActivityIntentTest {
+}
