dcdf16a8b7d18a82f4fce7a13116f76830a7053c
Karega K. Scott <kkscott@outlook.com>
2021-04-19 11:23:58 -0500

    App defined comparor

---
+++ app/src/main/java/karega/scott/checkers/util/BoardSquareComparor.java
@@ -0,0 +1,16 @@
+package karega.scott.checkers.util;
+
+import karega.scott.checkers.BoardSquareInfo;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class BoardSquareComparor implements Comparator<BoardSquareInfo> {
+	public int compare(BoardSquareInfo obj1, BoardSquareInfo obj2) {
+		throw new NotImplemented();
+	}
+
+	public boolean equals(Object obj) {
+		throw new NotImplemented();
+	}
+}
