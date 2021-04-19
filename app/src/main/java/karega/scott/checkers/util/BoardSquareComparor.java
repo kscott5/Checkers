4cf1c5ba8886aeccf6693f0d9f768f4dc6c6c018
Karega K. Scott <kkscott@outlook.com>
2021-04-19 12:05:48 -0500

    import exceptions

--- app/src/main/java/karega/scott/checkers/util/BoardSquareComparor.java
+++ app/src/main/java/karega/scott/checkers/util/BoardSquareComparor.java
@@ -2,15 +2,18 @@ package karega.scott.checkers.util;
 
 import karega.scott.checkers.BoardSquareInfo;
 
+import java.lang.Throwable;
+import java.lang.RuntimeException;
+
 import java.util.Arrays;
 import java.util.Comparator;
 
 public class BoardSquareComparor implements Comparator<BoardSquareInfo> {
 	public int compare(BoardSquareInfo obj1, BoardSquareInfo obj2) {
-		throws new Exception("Not implemented");
+		throw new RuntimeException("Not implemented");
 	}
 
 	public boolean equals(Object obj) {
-		throws new Exception("Not implemented");
+		throws new RuntimeException("Not implemented");
 	}
 }
