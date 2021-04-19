921043c360278c4da8031c75911d115aa963320b
Karega K. Scott <kkscott@outlook.com>
2021-04-19 11:40:35 -0500

    Unit test comparor

---
+++ app/src/test/java/karega/scott/checkers/util/test/BoardSquareComparorTest.java
@@ -0,0 +1,16 @@
+package karega.scott.checkers.util.test;
+
+import karega.scott.checkers.BoardSquareInfo;
+import karega.scott.checkers.util.BoardSquareComparor;
+
+import java.lang.Integer;
+import org.junit.Test;
+import org.junit.Assert;
+
+public class BoardSquareComparorTest {    
+    @Test public void compare() {
+	}
+	
+    @Test public void equals() {
+	}
+}
