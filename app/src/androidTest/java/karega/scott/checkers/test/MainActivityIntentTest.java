f4256935ee243de0e3aaf36c362b3287fca66e4f
Karega K. Scott <kkscott@outlook.com>
2021-04-18 19:40:52 -0500

    Stub UI test

--- app/src/androidTest/java/karega/scott/checkers/test/MainActivityIntentTest.java
+++ app/src/androidTest/java/karega/scott/checkers/test/MainActivityIntentTest.java
@@ -4,6 +4,8 @@ import karega.scott.checkers.MainActivity;
 import androidx.test.filters.MediumTest;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
+import androidx.test.espresso.intent.Intents;
+
 import org.junit.Before;
 import org.junit.After;
 import org.junit.Test;
@@ -13,4 +15,22 @@ import org.junit.Rule;
 public class MainActivityIntentTest {
 	@Rule
 	ActivityScenarioRule rule = new ActivityScenarioRule<>(MainActivity.class);
+
+	@Before
+	public void before() {
+		Intents.init();
+	}
+
+	@After
+	public void after() {
+		Intents.release();
+	}
+
+	@Test
+	public void newGamePassNPlay() {
+	}
+
+	@Test
+	public void newGameDevicePlay() {
+	}
 }
