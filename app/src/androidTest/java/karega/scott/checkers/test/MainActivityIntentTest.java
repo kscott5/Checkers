cf03ccb772c15612e8ffadbbb73a8ef30d72484a
Karega K. Scott <kkscott@outlook.com>
2021-04-18 19:35:02 -0500

    Define activity rule

--- app/src/androidTest/java/karega/scott/checkers/test/MainActivityIntentTest.java
+++ app/src/androidTest/java/karega/scott/checkers/test/MainActivityIntentTest.java
@@ -2,10 +2,15 @@ package karega.scott.checkers.test;
 import karega.scott.checkers.MainActivity;
 
 import androidx.test.filters.MediumTest;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
 import org.junit.Before;
+import org.junit.After;
 import org.junit.Test;
+import org.junit.Rule;
 
 @MediumTest
 public class MainActivityIntentTest {
+	@Rule
+	ActivityScenarioRule rule = new ActivityScenarioRule<>(MainActivity.class);
 }
