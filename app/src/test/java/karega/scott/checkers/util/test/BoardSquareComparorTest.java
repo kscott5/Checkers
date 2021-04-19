56bafa1ba7d00f10ef1a6e09083250ee6e296af9
Karega K. Scott <kkscott@outlook.com>
2021-04-19 11:54:41 -0500

    Implementation not available

--- app/src/test/java/karega/scott/checkers/util/test/BoardSquareComparorTest.java
+++ app/src/test/java/karega/scott/checkers/util/test/BoardSquareComparorTest.java
@@ -11,6 +11,20 @@ public class BoardSquareComparorTest {
     @Test public void compare() {
 	}
 	
-    @Test public void equals() {
+    @Test public void equalsTrue() {
+		final BoardSquareInfo bsi = new BoardSquareInfo(0,0,0,BoardGameEngine.EMPTY_STATE,BoardGameEngine.SQUARE_CHIP_START_ANGLE);
+
+		BoardSquareComparor compare = new BoardSquareComparor(bsi);
+		
+		Assert.assertTrue(compare.equals(bsi));
+	}
+
+	@Test public void equalsFalse) {
+		final BoardSquareComparor bsi0 = new BoardSquareInfo(0,0,BoardGameEngine.PLAYER1_STATE,BoardGameEngine.SQUARE_CHIP_START_ANGLE);
+		final BoardSquareComparor bsi1 = new BoardSquareInfo(1,0,BoardGameEngine.PLAYER2_STATE,BoardGameEngine.SQUARE_CHIP_START_ANGLE);
+
+		BoardSquareComparor compare = new BoardSquareComparor(bsi1);
+
+		Assert.assertFalse(compare.equals(bsi0));
 	}
 }
