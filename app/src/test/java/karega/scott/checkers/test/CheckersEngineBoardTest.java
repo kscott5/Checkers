756d978caa1493592df486e8cb26e5421ac9afbb
Karega K. Scott <kkscott@outlook.com>
2021-04-20 10:42:49 -0500

    Test board rows {5..7}

--- app/src/test/java/karega/scott/checkers/test/CheckersEngineBoardTest.java
+++ app/src/test/java/karega/scott/checkers/test/CheckersEngineBoardTest.java
@@ -101,4 +101,84 @@ public class CheckersEngineBoardTest {
 		Assert.assertTrue(engine.validateSelection(2,7));
 	}
 
+	@Test public void validateSelectionPlayer1Row5() {
+		Assert.assertTrue(engine.isPlayer1());
+
+		Assert.assertTrue(engine.validateSelection(5,0));
+		Assert.assertFalse(engine.validateSelection(5,1));
+		Assert.assertTrue(engine.validateSelection(5,2));
+		Assert.assertFalse(engine.validateSelection(5,3));
+		Assert.assertTrue(engine.validateSelection(5,4));
+		Assert.assertFalse(engine.validateSelection(5,5));
+		Assert.assertTrue(engine.validateSelection(5,6));
+		Assert.assertFalse(engine.validateSelection(5,7));
+	}
+
+	@Test public void validateSelectionPlayer2Row5() {
+		engine.switchPlayer();
+		Assert.assertTrue(engine.isPlayer2());
+
+		Assert.assertFalse(engine.validateSelection(5,0));
+		Assert.assertFalse(engine.validateSelection(5,1));
+		Assert.assertFalse(engine.validateSelection(5,2));
+		Assert.assertFalse(engine.validateSelection(5,3));
+		Assert.assertFalse(engine.validateSelection(5,4));
+		Assert.assertFalse(engine.validateSelection(5,5));
+		Assert.assertFalse(engine.validateSelection(5,6));
+		Assert.assertFalse(engine.validateSelection(5,7));
+	}
+
+	@Test public void validateSelectionPlayer1Row6() {
+		Assert.assertTrue(engine.isPlayer1());
+
+		Assert.assertFalse(engine.validateSelection(6,0));
+		Assert.assertTrue(engine.validateSelection(6,1));
+		Assert.assertFalse(engine.validateSelection(6,2));
+		Assert.assertTrue(engine.validateSelection(6,3));
+		Assert.assertFalse(engine.validateSelection(6,4));
+		Assert.assertTrue(engine.validateSelection(6,5));
+		Assert.assertFalse(engine.validateSelection(6,6));
+		Assert.assertTrue(engine.validateSelection(6,7));
+	}
+
+	@Test public void validateSelectionPlayer2Row6() {
+		engine.switchPlayer();
+		Assert.assertTrue(engine.isPlayer2());
+
+		Assert.assertFalse(engine.validateSelection(6,0));
+		Assert.assertFalse(engine.validateSelection(6,1));
+		Assert.assertFalse(engine.validateSelection(6,2));
+		Assert.assertFalse(engine.validateSelection(6,3));
+		Assert.assertFalse(engine.validateSelection(6,4));
+		Assert.assertFalse(engine.validateSelection(6,5));
+		Assert.assertFalse(engine.validateSelection(6,6));
+		Assert.assertFalse(engine.validateSelection(6,7));
+	}
+
+	@Test public void validateSelectionPlayer1Row7() {
+		Assert.assertTrue(engine.isPlayer1());
+
+		Assert.assertTrue(engine.validateSelection(7,0));
+		Assert.assertFalse(engine.validateSelection(7,1));
+		Assert.assertTrue(engine.validateSelection(7,2));
+		Assert.assertFalse(engine.validateSelection(7,3));
+		Assert.assertTrue(engine.validateSelection(7,4));
+		Assert.assertFalse(engine.validateSelection(7,5));
+		Assert.assertTrue(engine.validateSelection(7,6));
+		Assert.assertFalse(engine.validateSelection(7,7));
+	}
+
+	@Test public void validateSelectionPlayer2Row7() {
+		engine.switchPlayer();
+		Assert.assertTrue(engine.isPlayer2());
+
+		Assert.assertFalse(engine.validateSelection(7,0));
+		Assert.assertFalse(engine.validateSelection(7,1));
+		Assert.assertFalse(engine.validateSelection(7,2));
+		Assert.assertFalse(engine.validateSelection(7,3));
+		Assert.assertFalse(engine.validateSelection(7,4));
+		Assert.assertFalse(engine.validateSelection(7,5));
+		Assert.assertFalse(engine.validateSelection(7,6));
+		Assert.assertFalse(engine.validateSelection(7,7));
+	}
 }
