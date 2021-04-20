a21b6cba001925583eb3a7b2b18514a16643aaa9
Karega K. Scott <kkscott@outlook.com>
2021-04-20 16:44:56 -0500

    Before and After annonation.

--- app/src/test/java/karega/scott/checkers/test/Player1PawnTest.java
+++ app/src/test/java/karega/scott/checkers/test/Player1PawnTest.java
@@ -6,6 +6,9 @@ import karega.scott.checkers.BoardSquareInfo;
 import org.junit.Test;
 import org.junit.Assert;
 
+import org.junit.Before;
+import org.junit.After;
+
 public class Player1PawnTest {
 	CheckersEngine engine;
 
