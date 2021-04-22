3bdf72bb7c47c455724bb3d9ffd73008be4e76e3
Karega K. Scott <kkscott@outlook.com>
2021-04-22 13:59:47 -0500

    Update square with player1 data

---
+++ app/src/test/java/karega/scott/checkers/test/SimpleForwardOnlyMoveTest.java
@@ -0,0 +1,50 @@
+import karega.scott.checkers.test;
+
+import karega.scott.checkers.CheckersEngine;
+import karega.scott.checkers.BoardSquareInfo;
+
+import java.util.ArrayList;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+
+public class SimpleForwardOnlyMoveTest {
+	CheckersEngine engine;
+	ArrayList<BoardSquareInfo> squares;
+
+	@Before public void before() {
+		engine = new CheckersEngine(/*vsDevice*/false);
+		engine.newGame();
+		engine.setBoardSquaresEmpty();
+
+		squares = new ArrayList<BoardSquareInfo>();
+	}
+
+	@After public void after() {
+	}
+
+	@Test public void updateSquareStatePlayer1() {
+		Assert.assertTrue(engine.isPlayer1());
+
+		Assert.assertTrue(engine.updateSquareState(5,0,CheckersEngine.PLAYER1_STATE));
+		Assert.assertTrue(engine.updateSquareState(7,0,CheckersEngine.PLAYER1_STATE));
+		Assert.assertTrue(engine.updateSquareState(6,1,CheckersEngine.PLAYER1_STATE));
+		Assert.assertTrue(engine.updateSquareState(5,2,CheckersEngine.PLAYER1_STATE));
+		Assert.assertTrue(engine.updateSquareState(7,2,CheckersEngine.PLAYER1_STATE));
+		Assert.assertTrue(engine.updateSquareState(6,3,CheckersEngine.PLAYER1_STATE));
+		Assert.assertTrue(engine.updateSquareState(5,4,CheckersEngine.PLAYER1_STATE));
+		Assert.assertTrue(engine.updateSquareState(7,4,CheckersEngine.PLAYER1_STATE));
+		Assert.assertTrue(engine.updateSquareState(6,5,CheckersEngine.PLAYER1_STATE));
+		Assert.assertTrue(engine.updateSquareState(5,6,CheckersEngine.PLAYER1_STATE));
+		Assert.assertTrue(engine.updateSquareState(7,6,CheckersEngine.PLAYER1_STATE));
+		Assert.assertTrue(engine.updateSquareState(6,7,CheckersEngine.PLAYER1_STATE));
+	}
+
+	@Test public void Player2Middle() {
+		engine.switchPlayer();
+		Assert.assertTrue(engine.isPlayer2());
+	}
+}
+
