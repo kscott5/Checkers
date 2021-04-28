3666588c5654724ec6594c8f040c809da09aecb0
Karega K. Scott <kkscott@outlook.com>
2021-04-28 18:52:10 -0500

    Use CheckersEngine not BoardGameEngine.

--- app/src/main/java/karega/scott/checkers/DeviceTask.java
+++ app/src/main/java/karega/scott/checkers/DeviceTask.java
@@ -3,9 +3,6 @@ package karega.scott.checkers;
 
 import java.util.TimerTask;
 import java.util.concurrent.atomic.AtomicBoolean;
-import android.annotation.SuppressLint;
-import android.util.Log;
-
 
 /**
  * Task allows this device to take it turn. 
@@ -13,21 +10,16 @@ import android.util.Log;
  *
  */
 public class DeviceTask extends TimerTask {
-	private static final String LOG_TAG = "DeviceTask";
-	
-	private final BoardGameEngine engine;
+	private final CheckersEngine engine;
 	private final AtomicBoolean locked;
 	
-	public DeviceTask(BoardGameEngine engine) {
+	public DeviceTask(CheckersEngine engine) {
 		this.locked = new AtomicBoolean(false);
 		this.engine = engine;		
 	}
 	
-	@SuppressLint("NewApi")
 	@Override
 	public void run() {
-		Log.d(LOG_TAG, "Running...");
-		
 		while(engine.isDevice() && !locked.compareAndSet(false, true)) {			
 			engine.deviceMove(); // perform some work
 			locked.set(false); // prepare to unlock
