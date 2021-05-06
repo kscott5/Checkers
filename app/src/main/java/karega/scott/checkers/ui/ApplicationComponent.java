f2ffd9ce5d4e00f6cdba3c3ff405127c79686ee8
Karega K. Scott <kkscott@outlook.com>
2021-05-06 13:21:27 -0500

    Prepare dragger integration.

---
+++ app/src/main/java/karega/scott/checkers/ui/ApplicationComponent.java
@@ -0,0 +1,10 @@
+package karega.scott.checkers.ui;
+
+import dragger;
+
+/* 
+ * ApplicationComponent that defines a @link dragger Application graph
+ */
+@Component
+public interface ApplicationComponent {
+}
