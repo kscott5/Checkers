420568c5db3ba7522574197ba26cf6fc5bd6b17c
Karega K. Scott <kkscott@outlook.com>
2021-05-06 18:03:00 -0500

    Simplfy on intercept motion event.

--- app/src/main/java/karega/scott/checkers/ui/BoardGridView.java
+++ app/src/main/java/karega/scott/checkers/ui/BoardGridView.java
@@ -38,65 +38,19 @@ public class BoardGridView extends GridView {
 	int row = -1, column = -1, viewWidth = 38;
 	@Override
 	public boolean onInterceptTouchEvent(MotionEvent event) {
-		int id = event.getPointerId(0);
 		int action = event.getActionMasked();
-		int index = event.getActionIndex();
-		int size = event.getHistorySize();
 		float x = event.getX(id), y = event.getY(id);
 
-		Log.d(LOG_TAG, "on Intercept Touch:          Id->" + id);
-		Log.d(LOG_TAG, "on Intercept Touch:       Index->" + index);
-		Log.d(LOG_TAG, "on Intercept Touch: HistorySize->" + size);
-		Log.d(LOG_TAG, "on Intercept Touch:           X->" + x);
-		Log.d(LOG_TAG, "on Intercept Touch:           Y->" + y);
-		Log.d(LOG_TAG, "On Intercept Touch:      height->" + this.getWidth());	
-		Log.d(LOG_TAG, "On Intercept Touch:       width->" + this.getWidth());	
-		Log.d(LOG_TAG, "On Intercept Touch:      action->" + event.actionToString(action));	
-
-		int viewWidth = 38; // Must use width from CheckerBoardSquare View
-		int tmpx = (int)Math.floor(y/viewWidth); // Sequence, order, of events
-		int tmpy = (int)Math.floor(x/viewWidth); // are important
-			
-		BoardAdapter adapter = (BoardAdapter)this.getAdapter();
-
-		switch(action) {
-			case MotionEvent.ACTION_DOWN:
-				if(row == -1 && column == -1) {
-					row = tmpx; column = tmpy;
-					BoardSquareInfo square = BoardActivity.gameEngine.getData(row,column);
-					Log.d(LOG_TAG, "Down->" + square);
-					BoardActivity.gameEngine.updateGameBoard(square.id, /*hasMore*/ true);
-				}
-				break;
-
-			case MotionEvent.ACTION_MOVE:
-				if(tmpx != row && tmpy != column) {
-					row = tmpx; column = tmpy;
-					BoardSquareInfo square = BoardActivity.gameEngine.getData(row,column);
-					Log.d(LOG_TAG, "Move->" + square);
-					BoardActivity.gameEngine.updateGameBoard(square.id, /*hasMore*/ true);
-				}
-				break;
-
-			case MotionEvent.ACTION_UP:
-				if(tmpx != row && tmpy != column) {
-					row = tmpx; column = tmpy;
-					BoardSquareInfo square = BoardActivity.gameEngine.getData(row,column);
-					Log.d(LOG_TAG, "Up->" + square);
-					BoardActivity.gameEngine.updateGameBoard(square.id, /*hasMore*/ false);
-				}
-
-				row = -1; column = -1;
-				break;
-
-			case MotionEvent.ACTION_CANCEL:
-			default:
-				row = -1; column = -1;
-				Log.d(LOG_TAG, "Cancel");
-				break;
+		BoardSquareInfo square = BoardActivity.gameEngine.getData(y,x,this.getColumnWidth());
+		Log.d(LOG_TAG, "On Intercept Touch [" + event.actionToString(action) + "] square->" + square);
+		
+		if(action == MotionEvent.ACTION_CANCEL) {
+			BoardActivity.gameEngine.saveSelectionReset();
+		} else {
+			boolean hasMore = (action == MotionEvent.ACTION_DOWN || action == MotionEvent.ACTION_MOVE);
+			BoardActivity.gameEngine.updateGameBoard(square.id, hasMore);
 		}
 
-		event.setAction(MotionEvent.ACTION_OUTSIDE);
 		return super.onInterceptTouchEvent(event);
 	}
 }
