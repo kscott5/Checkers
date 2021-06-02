62855a096939ea6bfd7304fa4fe8cd41241ebd93
Karega K. Scott <kkscott@outlook.com>
2021-06-02 11:07:08 -0500

    Correct import statements;

--- app/src/main/java/karega/scott/checkers/ui/CheckerBoardView.java
+++ app/src/main/java/karega/scott/checkers/ui/CheckerBoardView.java
@@ -10,9 +10,13 @@ import android.util.Log;
 import android.util.AttributeSet;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 
 import android.view.View;
 import android.view.MotionEvent;
+import android.view.View.OnTouchListener;
 
 public class CheckerBoardView extends View implements OnTouchListener {
 	public static final String LOG_TAG = "CheckerBoardView";
